1d40f21d815cc899aacf7c69b4e66b891201a0b8
Toki <55911995+tokio-k@users.noreply.github.com>
2021-04-30 07:23:52 +0900

    init

---
+++ nextjs/src/pages/_app.tsx
@@ -0,0 +1,8 @@
+import "/src/styles/globals.css"
+import type { AppProps } from "next/app";
+
+const App = (props: AppProps) => {
+  return <props.Component {...props.pageProps} />;
+};
+
+export default App;
---
+++ nextjs/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+const Home = () => {
+  return (
+    <>
+    <h1>
+      Hello World
+    </h1>
+    </>
+  );
+};
+
+export default Home;
