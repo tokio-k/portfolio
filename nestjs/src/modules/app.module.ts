2d8c5061c1727a3e7360c0e80c1ec627a987d46e
Toki <55911995+tokio-k@users.noreply.github.com>
2021-05-01 07:02:37 +0900

    init

---
+++ nestjs/src/modules/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot({
+      playground: true,
+      autoSchemaFile: 'schema.graphql',
+    }),
+  ],
+})
+export class AppModule {}
