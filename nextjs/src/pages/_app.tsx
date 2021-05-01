2337796d5ecc16ccf82348850e8daea8625f5b96
Toki <55911995+tokio-k@users.noreply.github.com>
2021-05-02 00:10:03 +0900

    ADD/Nextjs-GraphQL

--- nextjs/src/pages/_app.tsx
+++ nextjs/src/pages/_app.tsx
@@ -1,8 +1,15 @@
-import "/src/styles/globals.css"
+import "/src/styles/globals.css";
+
+import { ApolloProvider } from "@apollo/client";
 import type { AppProps } from "next/app";
+import { apolloClient } from "src/apollo/apolloClient";
 
 const App = (props: AppProps) => {
-  return <props.Component {...props.pageProps} />;
+  return (
+    <ApolloProvider client={apolloClient}>
+      <props.Component {...props.pageProps} />;
+    </ApolloProvider>
+  );
 };
 
 export default App;
--- nextjs/src/pages/index.tsx
+++ nextjs/src/pages/index.tsx
@@ -1,13 +1,30 @@
-import Head from "next/head";
+import gql from "graphql-tag";
+import { useSampleQuery } from "src/apollo/graphql";
 
 const Home = () => {
+  const { data, loading } = useSampleQuery({ variables: { id: 2 } });
   return (
     <>
-    <h1>
-      Hello World
-    </h1>
+      {!loading && (
+        <>
+          <p>{data?.sample?.id}</p>
+          <p>{data?.sample?.name}</p>
+          <p>{data?.sample?.freeInput}</p>
+        </>
+      )}
+      <h1>Hello World</h1>
     </>
   );
 };
 
 export default Home;
+
+gql`
+  query sample($id: Int!) {
+    sample(id: $id) {
+      id
+      name
+      freeInput
+    }
+  }
+`;
