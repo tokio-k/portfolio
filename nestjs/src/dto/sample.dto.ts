7c157c98b5f8535e8fc968f33de4b731369fb0a2
Toki <55911995+tokio-k@users.noreply.github.com>
2021-07-19 20:51:17 +0900

    first commit

--- nestjs/src/dto/sample.dto.ts
+++
@@ -1,9 +0,0 @@
-import { Field, InputType } from '@nestjs/graphql';
-
-@InputType()
-export class AddSampleDto {
-  @Field()
-  name: string;
-  @Field()
-  freeInput: string;
-}
--- nestjs/src/modules/sample.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SampleResolver } from 'src/resolvers/sample.resolver';
-
-@Module({
-  providers: [SampleResolver],
-})
-export class SampleModule {}
