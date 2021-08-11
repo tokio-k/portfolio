a774a46e11e9d7babf1b799cb220c1d597ad3012
Toki <55911995+tokio-k@users.noreply.github.com>
2021-08-11 10:18:04 +0900

    update/プロフィール写真追加

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,11 +1,19 @@
+import Image from "next/image";
 import { Title } from "src/components/shared/Title";
 
 export const About = () => {
   return (
     <div className=" min-h-screen flex flex-col ">
       <Title title={"ABOUT"} />
-      <div className="flex place-content-center items-center flex-auto pb-40 font-bold sm:text-xl ">
-        {/* todo image作成 */}
+      <div className="flex place-content-center items-center flex-auto pb-40 font-bold sm:text-xl flex-col md:flex-row gap-8 ">
+        <div className="">
+          <Image
+            src="/profile.jpg"
+            width="200"
+            height="200"
+            className="rounded-full"
+          />
+        </div>
         <div>
           <p>愛知県の文系大学3年生です。</p>
           <p>大学1年生の時にプログラミングを始め</p>
