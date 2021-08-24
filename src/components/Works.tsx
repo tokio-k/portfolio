9bff796c9d24f7fc18e9ea3c59e833bc9a895006
Toki <55911995+tokio-k@users.noreply.github.com>
2021-08-25 07:47:13 +0900

    update/worksのコンポーネント化

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -1,31 +1,50 @@
 import type { VFC } from "react";
 import { Title } from "src/components/shared/Title";
 
-const LONG_INTERN = [
+type WorkList = {
+  companyName: string;
+  period: string;
+  overview: string;
+  skills: string;
+  doing: boolean;
+}[];
+
+const LONG_INTERN: WorkList = [
   {
     companyName: "株式会社SNAPSHOT",
-    skills: "Java、SpringBoot、jQuery、postgres、oracle",
+    period: "2020/05~2021/03",
     overview:
       "勤怠管理サービスの開発インターン。設計書を基に機能追加や改修の実装をしていました。フロントエンドから、バックエンド、DBまでの一連の流れを機能ごとに実装していました。",
-    period: "2020/05~2021/03",
+    skills: "Java、SpringBoot、jQuery、postgres、oracle",
     doing: false,
   },
   {
     companyName: "株式会社OnePile",
-    skills: "TypeScript、React、Next.js、Git",
+    period: "2021/04~",
     overview:
       "学生が起業したベンチャーで業務委託として開発。エンジニアは学生のみ(フロントエンド2人)。機能追加やSEO対策、パフォーマンス改善、バグ改修などフロントエンド開発全般を担当。",
-    period: "2021/04~",
+    skills: "TypeScript、React、Next.js、Git",
     doing: true,
   },
 ];
 
-type CardType = {
+const SHORT_INTERN: WorkList = [
+  {
+    companyName: "サイボウズ株式会社",
+    period: "2021/08/23~2021/08/27",
+    overview:
+      "1週間のサマーインターン。参加学生6名。サイボウズ株式会社が提供するkintoneを題材として、新機能のプロトタイプを開発。",
+    skills: "Java、JavaScript",
+    doing: false,
+  },
+];
+
+type CardContentType = {
   title: string;
   content: string;
 };
 
-const CardContent: VFC<CardType> = (props) => {
+const WorkCardContent: VFC<CardContentType> = (props) => {
   return (
     <div className="flex">
       <span className="flex-none font-bold">{props.title}：</span>
@@ -34,40 +53,78 @@ const CardContent: VFC<CardType> = (props) => {
   );
 };
 
+type CardType = {
+  companyName: string;
+  period: string;
+  overview: string;
+  skills: string;
+  doing: boolean;
+};
+
+const WorkCard: VFC<CardType> = ({
+  companyName,
+  period,
+  overview,
+  skills,
+  doing,
+}) => {
+  return (
+    <div
+      className={`${
+        doing ? "border-2 border-red-500" : "border border-gray-300"
+      }
+      text-left bg-gray-50 p-5 w-full max-w-sm flex flex-col gap-y-3 shadow-md relative`}
+    >
+      {doing && (
+        <div className="absolute top-0 right-0 text-red-500 p-2 font-bold">
+          所属中
+        </div>
+      )}
+      <WorkCardContent title="会社名" content={companyName} />
+      <WorkCardContent title="期間" content={period} />
+      <WorkCardContent title="概要" content={overview} />
+      <WorkCardContent title="使用技術" content={skills} />
+    </div>
+  );
+};
+
+type CardListType = {
+  title: string;
+  workList: WorkList;
+};
+
+const WorkCardList: VFC<CardListType> = ({ title, workList }) => {
+  return (
+    <div className="mb-5">
+      <h3 className="font-bold text-lg py-3">{title}</h3>
+      <div className="flex flex-wrap place-content-around">
+        {workList.map((work, i) => {
+          return (
+            <div key={i} className="m-5">
+              <WorkCard
+                companyName={work.companyName}
+                period={work.period}
+                overview={work.overview}
+                skills={work.skills}
+                doing={work.doing}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
 export const Works = () => {
   return (
     <div className="min-h-screen">
       <Title title="WORKS" />
-      <div>
-        <h4 className="font-bold text-lg py-3">
-          就業型長期インターン&アルバイト
-        </h4>
-        <div className="flex flex-wrap place-content-around">
-          {LONG_INTERN.map(
-            ({ companyName, skills, overview, period, doing }, i) => {
-              return (
-                <div
-                  key={i}
-                  className={`${
-                    doing ? "border-2 border-red-500" : "border border-gray-300"
-                  }
-                          text-left bg-white p-5 w-full max-w-sm flex flex-col gap-y-3 m-5 shadow-md relative`}
-                >
-                  {doing && (
-                    <div className="absolute top-0 right-0 text-red-500 p-2 font-bold">
-                      所属中
-                    </div>
-                  )}
-                  <CardContent title="会社名" content={companyName} />
-                  <CardContent title="期間" content={period} />
-                  <CardContent title="概要" content={overview} />
-                  <CardContent title="使用技術" content={skills} />
-                </div>
-              );
-            }
-          )}
-        </div>
-      </div>
+      <WorkCardList
+        title="就業型長期インターン&アルバイト"
+        workList={LONG_INTERN}
+      />
+      <WorkCardList title="短期/中期インターンシップ" workList={SHORT_INTERN} />
     </div>
   );
 };
